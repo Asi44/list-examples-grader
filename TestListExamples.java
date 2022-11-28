f16140def8dd4013a3a9f4d8674ae8229b00566e
Asi44 <aasiyah.shaikh44@gmail.com>
2022-11-27 21:16:51 -0800

    wrote lab report 5

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,82 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
 
+class startsWithA implements StringChecker{
+    
+    public boolean checkString(String s) {
+        if (s.substring(0, 1).equals("a") || s.substring(0, 1).equals("A")) {
+            return true;
+        }
+        return false;
+    }
+
+}
+
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
+
   // Write your grading tests here!
+
+  @Test(timeout = 1000)
+  public void testFilter() {
+        List<String> list1 = new ArrayList<>();
+        startsWithA aStart = new startsWithA();
+        list1.add("apple");
+        list1.add("ball");
+        list1.add("cat");
+        list1.add("ant");
+        List<String> list2 = new ArrayList<>();
+        list2.add("apple");
+        list2.add("ant");
+
+        assertEquals(list2, ListExamples.filter(list1, aStart));
+       
+    }
+
+    @Test(timeout = 1000)
+    public void testFilterEmptyResult() {
+        List<String> list1 = new ArrayList<>();
+        startsWithA aStart = new startsWithA();
+        list1.add("banana");
+        list1.add("melon");
+        list1.add("pineapple");
+          
+        List<String> list2 = new ArrayList<>();
+        assertEquals(list2, ListExamples.filter(list1, aStart));
+      }
+
+    @Test (timeout = 1000)
+    public void testMerge() {
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        list1.add("a");
+        list1.add("d");
+        list1.add("e");
+        list2.add("b");
+        list2.add("c");
+        expected.add("a");
+        expected.add("b");
+        expected.add("c");
+        expected.add("d");
+        expected.add("e");
+        
+        assertEquals(expected, ListExamples.merge(list1, list2));
+    }
+
+    @Test (timeout = 1000)
+    public void testMergeEmptyList() {
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        list1.add("a");
+        list1.add("d");
+        list1.add("e");
+        expected.add("a");
+        expected.add("d");
+        expected.add("e");
+        
+        assertEquals(expected, ListExamples.merge(list1, list2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,67 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
 
+GRADE=0
+
+if [[ -f ListExamples.java ]]; then
+  echo "file found"
+else
+  echo "ListExamples.java file not found"
+  echo "Grade: "$GRADE
+  exit
+fi
+
+cd ~
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+cd student-submission
+
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+
+if [[ $? -ne 0 ]]; then
+  echo "Compilation failed"
+  echo "Grade: "$GRADE
+  exit
+fi
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > passed.txt       
+
+cat passed.txt
+
+RESULT=`grep -c "OK (4 tests)" passed.txt`
+if [[ $RESULT -eq 1 ]]; then
+        GRADE=4
+        echo "Grade: "$GRADE" out of 4"
+        exit
+fi
+
+RESULT=`grep -c "Tests run: 4,  Failures: 1" passed.txt`
+if [[ $RESULT -eq 1 ]]; then
+        GRADE=3
+        echo "Grade: "$GRADE" out of 4"
+        exit
+fi
+
+RESULT=`grep -c "Tests run: 4,  Failures: 2" passed.txt`
+if [[ $RESULT -eq 1 ]]; then
+        GRADE=2
+        echo "Grade: "$GRADE" out of 4"
+        exit
+fi
+
+RESULT=`grep -c "Tests run: 4,  Failures: 3" passed.txt`
+if [[ $RESULT -eq 1 ]]; then
+        GRADE=1
+        echo "Grade: "$GRADE" out of 4"
+        exit
+fi
+
+RESULT=`grep -c "Tests run: 4,  Failures: 4" passed.txt`
+if [[ $RESULT -eq 1 ]]; then
+        GRADE=0
+        echo "Grade: "$GRADE" out of 4"
+        exit
+fi
